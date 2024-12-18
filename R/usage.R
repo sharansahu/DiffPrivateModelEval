64b5a4965b098ba4db58a6caafbf422c1f184410
Sharan Sahu <ssahu01@berkeley.edu>
2024-12-18 06:47:38 -0800

    added vignette and new usage script

--- R/usage.R
+++ R/usage.R
@@ -1,7 +1,6 @@
-library(here)
-
-source(here::here("R/wrapper_functions.R"))
-source(here::here("R/visualization_functions.R"))
+devtools::install_github("sharansahu/DiffPrivateModelEval")
+library(DiffPrivateModelEval)
+library(ggplot2)
 
 dp_algo <- create_DP_LSVI_Ngo(H = 15, K = 50)
 lsvi_algo <- create_LSVI_UCB(H = 15, K = 50)
@@ -13,9 +12,6 @@ dp_algo$run()
 lsvi_algo$run()
 dp_algo1$run()
 lsvi_algo1$run()
-
-# Plot individual cumulative regret
-plot_regret(lsvi_algo)
 
 # Plot multiple algorithm comparison
 plot_multiple_algos(list(dp_algo, lsvi_algo, dp_algo1, lsvi_algo1))
